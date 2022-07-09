3b7e276924e5c31cac56a19344ab4ef485e11206
Daniwalls97 <dps7597@gmail.com>
2022-07-09 20:36:14 +0100

    Add background home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { createUseStyles } from 'react-jss';
+import background from "../assets/img/ggshopwallpaper.jpg";
 
 export const Home = () => {
-    return <div >
-        Bienvenidos a GGShop
-    </div>
+    const styles = HomeStyles();
+
+    return <div className={styles.container} />
 }
+
+const HomeStyles = createUseStyles({
+    container: {
+        backgroundImage: `url(${background})`,
+        height: "100vh",
+        backgroundSize: "cover",
+    }
+  });
